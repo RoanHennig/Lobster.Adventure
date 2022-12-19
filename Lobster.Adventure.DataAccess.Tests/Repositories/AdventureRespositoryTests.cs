418a595bfa66cadb00513105e119303d3da693bf
RoanHennig <roanhennig@gmail.com>
2022-12-19 14:28:39 +0200

    removed commented code

--- Lobster.Adventure.DataAccess.Tests/Repositories/AdventureRespositoryTests.cs
+++ Lobster.Adventure.DataAccess.Tests/Repositories/AdventureRespositoryTests.cs
@@ -19,28 +19,4 @@ public class AdventureRespositoryTests
         //Assert
         mockadventureCollection.Verify(service => service.InsertOne(request, It.IsAny<InsertOneOptions>(), CancellationToken.None), Times.Once());
     }
-
-    //[Fact()]
-    //public void Read_OnSuccess_CollectionFindExactlyOnce()
-    //{
-    //    //Arrange
-    //    var mockadventureCollection = new Mock<IMongoCollection<LobsterAdventureEntity>>();
-
-    //    var adventureRespository = new AdventureMongoDbRespository(mockadventureCollection.Object);
-
-    //    var request = LobsterAdventureKeyFixtures.GetKey();
-
-    //    var filter = Builders<LobsterAdventureEntity>.Filter.Eq("userId", request.UserId) &
-    //                Builders<LobsterAdventureEntity>.Filter.Eq("name", request.Name);
-
-    //    mockadventureCollection.Setup(service => service.FindSync(filter, null, CancellationToken.None)).Returns(new LobsterAdventureAsyncCurser());
-
-    //    //Act
-
-    //    var result = adventureRespository.Read(request);
-
-    //    //Assert
-
-    //    mockadventureCollection.Verify(service => service.FindSync(filter, null, CancellationToken.None), Times.Once());
-    //}
 }
